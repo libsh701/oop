7f802a8751dcd8066ae04b987ea26229c46bee09
libsh701 <72676565+libsh701@users.noreply.github.com>
2022-05-15 14:09:34 +0200

    Initial Commit

---
+++ Maze/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Maze
+//
+//  Created by Bashar Levin on 2022-05-15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
